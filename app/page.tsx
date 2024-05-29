16c30b85f50a3ff6a8d624871f658e9f47f39019
cout-sid <sidnike19@gmail.com>
2024-05-29 16:39:24 +0530

    styling home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 import {Hero} from '@/components';
+import {CustomButton} from '@/components';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+      
     </main>
   );
 }
